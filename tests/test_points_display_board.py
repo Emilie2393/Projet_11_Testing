5e41497c02ab4f3c970a3d92caa5a59fecd59dde
Emily2393 <emilie.rich4rd@gmail.com>
2024-05-02 14:51:34 +0200

    test conftest and server update

---
+++ tests/test_points_display_board.py
@@ -0,0 +1,8 @@
+from tests.conftest import client
+
+class TestClubPointsUpdate:
+
+	def test_should_status_code_ok(self, client):
+		response = client.get('/')
+		assert response.status_code == 200
+		assert 'Club name' in response.data.decode() 
