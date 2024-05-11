0db740eab16c104a45b32dba04dd54f89e03c3c1
Emily2393 <emilie.rich4rd@gmail.com>
2024-05-11 12:56:18 +0200

    TestClubPointsDisplayed improved

--- tests/test_points_display_board.py
+++ tests/test_points_display_board.py
@@ -1,8 +1,8 @@
 from tests.conftest import client
 
-class TestPointsDiplayBoard:
+class TestClubPointsDiplayed:
 
-	def test_should_status_code_ok(self, client):
+	def test_club_points_displayed(self, client):
 		response = client.get('/')
 		assert response.status_code == 200
 		assert 'Club name' in response.data.decode() 
